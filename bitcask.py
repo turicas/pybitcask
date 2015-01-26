fe05d1bcb6c64fb1fc2231fbf2cbf6e9fda10956
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-01-26 10:52:43 -0200

    Add .get/.set tests and implementation

---
+++ bitcask.py
@@ -0,0 +1,132 @@
+# coding: utf-8
+
+# Copyright 2015 Álvaro Justen
+#
+# This file is part of pybitcask.
+# You can get more information at: https://github.com/turicas/pybitcask
+#
+# pybitcask is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or # (at your option) any later
+# version.
+#
+# pybitcask is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pybitcask. If not, see <http://www.gnu.org/licenses/>.
+
+from collections import namedtuple
+from struct import Struct
+from time import time
+from zlib import crc32
+
+
+FIRST_STRUCT = Struct('>IHI')
+SECOND_STRUCT = Struct('>i')
+HEADER_STRUCT = Struct('>iIHI')
+HEADER_SIZE = 4 + 4 + 2 + 4  # crc + timestamp + key_size + value_size
+Hint = namedtuple('Hint', ['file_id', 'value_size', 'value_position',
+                           'timestamp'])
+
+
+def pack_data(key, value):
+    """Pack a key-value pair to store on the data/log file
+
+    Format: [crc][timestamp][ksz][value_sz][key][value]
+
+    Each field having the following sizes (from include/bitcask.hrl):
+
+    - crc: signed int, 32 bits (4 bytes), struct format: i
+    - timestamp: unsigned int, 32 bits (4 bytes), struct format: I
+    - key_size: unsigned int, 16 bits (2 bytes), struct format: H
+    - value_size: unsigned int, 32 bits (4 bytes), struct format: I
+
+    Erlang uses big endian by default, so our struct format starts with '>'
+
+    Struct formats we use:
+    - H = unsigned short int (2 bytes)
+    - i = int (4 bytes)
+    - I = unsigned int (4 bytes)
+    """
+
+    value_size, timestamp = len(value), int(time())
+    data = FIRST_STRUCT.pack(timestamp, len(key), value_size) + key + value
+    entry = SECOND_STRUCT.pack(crc32(data)) + data
+    return entry, value_size, timestamp
+
+def read_hint_from_data_file(fobj):
+    header = fobj.read(HEADER_SIZE)
+    if header == '':
+        raise RuntimeError()
+
+    # TODO: test corrupted data (struct can't unpack)
+    crc, timestamp, key_size, value_size = HEADER_STRUCT.unpack(header)
+    key = fobj.read(key_size)
+    if header == '':
+        raise RuntimeError()
+
+    value_position = fobj.tell()
+    fobj.seek(value_size, 1)  # whence=1 => relative position = current
+    # TODO: check CRC
+    return timestamp, value_position, value_size, key
+
+
+class Bitcask:
+    def __init__(self, filename):
+        # TODO: add read-write option (default = read-only)
+        # TODO: add flush option (default = ?)
+        # TODO: a bitcask should be an entire directory, not a file
+        self.__fobj = open(filename, 'a+b')
+        self.flush = True
+        self.__make_keydir()
+
+    def __make_keydir(self):
+        """Traverses active file to create the in-RAM keydir"""
+        fobj = self.__fobj
+        fobj.seek(0)
+        file_id = fobj.name
+        self.__keydir = {}
+
+        while True:
+            try:
+                timestamp, value_position, value_size, key = \
+                        read_hint_from_data_file(fobj)
+            except RuntimeError:
+                break
+            else:
+                self.__keydir[key] = Hint(file_id, value_size, value_position,
+                                          timestamp)
+        fobj.seek(0)
+
+    def set(self, key, value):
+        """Append pair entry to active file, update keydir and hint file"""
+
+        fobj = self.__fobj
+        data, value_size, timestamp = pack_data(key, value)
+
+        fobj.seek(0, 2)  # whence=2 => relative position = EOF
+        fobj.write(data)
+        if self.flush:
+            self.__fobj.flush()
+
+        value_position = self.__fobj.tell() - value_size
+        hint = Hint(self.__fobj.name, value_size, value_position, timestamp)
+        self.__keydir[key] = hint
+
+        # TODO: update hint file
+
+    def get(self, key):
+        """Get hint from keydir and load data from disk"""
+
+        hint = self.__keydir[key]
+        fobj = self.__fobj
+        fobj.seek(hint.value_position)
+        return fobj.read(hint.value_size)
+
+    def close(self):
+        """Close active file"""
+
+        self.__fobj.flush()
+        self.__fobj.close()
---
+++ test_bitcask.py
@@ -0,0 +1,203 @@
+# coding: utf-8
+
+# Copyright 2015 Álvaro Justen
+#
+# This file is part of pybitcask.
+# You can get more information at: https://github.com/turicas/pybitcask
+#
+# pybitcask is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or # (at your option) any later
+# version.
+#
+# pybitcask is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pybitcask. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import random
+import struct
+import tempfile
+import time
+import unittest
+import uuid
+import zlib
+
+from bitcask import Bitcask, pack_data
+
+
+VALUE_LENGTH = 2 ** 10
+KV_COUNT = 10 ** 3
+
+def random_string(size=1048576):  # default size is 1MB
+    return os.urandom(size)
+
+class TestBitcask(unittest.TestCase):
+
+    files_to_delete = []
+
+    def tearDown(self):
+        for filename in self.files_to_delete:
+            try:
+                os.unlink(filename)
+            except OSError:
+                pass
+
+    def test_write_one_key_value_pair(self):
+        # TODO: should test pack_data
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.close()
+        self.files_to_delete.append(temp_file.name)
+
+        key = b'some key'
+        value = b'some value'
+
+        kv_store = Bitcask(temp_file.name)
+        kv_store.set(key, value)
+        kv_store.close()
+
+        timestamp = int(time.time())
+        key_size = len(key)
+        value_size = len(value)
+        data = struct.pack('>IHI', timestamp, key_size, value_size) + key + \
+               value
+        crc = zlib.crc32(data)
+        expected_result = struct.pack('>i', crc) + data
+
+        with open(temp_file.name, 'rb') as fobj:
+            self.assertEqual(fobj.read(), expected_result)
+
+    def test_write_more_than_one_key_value_pair_at_once(self):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.close()
+        self.files_to_delete.append(temp_file.name)
+
+        keys_values = [(b'eggs', b'spam'), (b'ham', '42'),
+                       (b'monty', b'python'), (b'spam', random_string())]
+
+        packed_data = b''
+        kv_store = Bitcask(temp_file.name)
+        for key, value in keys_values:
+            data, _, _ = pack_data(key, value)
+            packed_data += data
+            kv_store.set(key, value)
+        kv_store.close()
+
+        with open(temp_file.name, 'rb') as fobj:
+            self.assertEqual(fobj.read(), packed_data)
+
+    def test_write_more_than_one_key_value_pair_twice(self):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.close()
+        self.files_to_delete.append(temp_file.name)
+
+        keys_values = [(b'eggs', b'python'), (b'ham', '42'),
+                       (b'monty', b'spam'), (b'spam', random_string())]
+        packed_data = b''
+
+        middle = int(len(keys_values) / 2)
+        kv_store = Bitcask(temp_file.name)
+        for key, value in keys_values[:middle]:
+            data, _, _ = pack_data(key, value)
+            packed_data += data
+            kv_store.set(key, value)
+        kv_store.close()
+
+        kv_store = Bitcask(temp_file.name)
+        for key, value in keys_values[middle:]:
+            data, _, _ = pack_data(key, value)
+            packed_data += data
+            kv_store.set(key, value)
+        kv_store.close()
+
+        with open(temp_file.name, 'rb') as fobj:
+            self.assertEqual(fobj.read(), packed_data)
+
+    def test_read_keys(self):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        self.files_to_delete.append(temp_file.name)
+        fobj = temp_file.file
+
+        # Generate random key-value pairs, pack it and save to temp file
+        packed_data = b''
+        kv_data = {}
+        for i in range(KV_COUNT):
+            key, value = uuid.uuid4().bytes, random_string(VALUE_LENGTH)
+            kv_data[key] = value
+            data, _, _ = pack_data(key, value)
+            packed_data += data
+        fobj.write(packed_data)
+        fobj.close()
+
+        # Check every key-value pair using bitcask read API
+        kv_store = Bitcask(temp_file.name)
+        for key, value in kv_data.items():
+            self.assertEqual(kv_store.get(key), value)
+
+        # test KeyError for non-existing keys
+        unexisting_keys = [uuid.uuid4().bytes for x in range(KV_COUNT)]
+        for key in unexisting_keys:
+            with self.assertRaises(KeyError):
+                kv_store.get(key)
+
+        kv_store.close()
+
+    def test_overwrite_keys(self):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.close()
+        self.files_to_delete.append(temp_file.name)
+
+        kv_store = Bitcask(temp_file.name)
+
+        # Generate random key-value pairs and write them to Bitcask
+        keys = []
+        for i in range(KV_COUNT):
+            key, value = uuid.uuid4().bytes, random_string(VALUE_LENGTH)
+            kv_store.set(key, value)
+            keys.append(key)
+
+        # Rewrite every value
+        data = {}
+        for key in keys:
+            new_value = random_string(VALUE_LENGTH)
+            kv_store.set(key, new_value)
+            data[key] = new_value
+
+        # Check every key-value pair using bitcask read API
+        for key, value in data.items():
+            self.assertEqual(kv_store.get(key), value)
+
+        kv_store.close()
+
+    # TODO: use Python mapping API instead of .get/.set methods
+
+    # TODO: test MAXKEYSIZE (16 bits) and MAXVALSIZE (32 bits)
+    # TODO: test MAXOFFSET = 16#7fffffffffffffff (max 63-bit unsigned)
+
+    # TODO: test directory creation with files
+    #       - files:
+    #         * '{:timestamp}.bitcask.data' (one or more)
+    #         * '{:timestamp}.bitcask.hint'
+    #         * 'bitcask.write.lock' when writing
+
+    # TODO: test deletion
+
+    # TODO: create hint file
+    # Hint file entry:
+    #  [<<Tstamp:?TSTAMPFIELD, KeySz:?KEYSIZEFIELD, TotalSz:?TOTALSIZEFIELD,
+    #   TombInt:?TOMBSTONEFIELD_V2, Offset:?OFFSETFIELD_V2>>, Key].
+    # Where:
+    # - TSTAMPFIELD          = 32 bits
+    # - KEYSIZEFIELD         = 16 bits
+    # - TOTALSIZEFIELD       = 32 bits
+    # - TOMBSTONEFIELD_V2    =  1 bit
+    # - OFFSETFIELD_V2       = 63 bits
+    # Total = 18 bytes (HINT_RECORD_SZ)
+    # Also: last (or first?) entry could be a file CRC (check read_crc on
+    # bitcask_fileops)
+
+    # TODO: implement merging process (create 'bitcask.merge.lock' when
+    #       merging)
