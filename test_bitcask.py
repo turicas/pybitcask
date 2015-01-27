4e5f9c7d40e4c233bbacc7bb862aee97e159a931
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-01-27 12:53:56 -0200

    Implement delete operation. Fix #4

--- test_bitcask.py
+++ test_bitcask.py
@@ -239,6 +239,38 @@ class TestBitcask(unittest.TestCase):
 
         kv_store.close()
 
+    def test_delete(self):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.close()
+        self.files_to_delete.append(temp_file.name)
+
+        kv_store = Bitcask(temp_file.name)
+
+        # Generate random key-value pairs and write them to Bitcask
+        for i in range(KV_COUNT):
+            key, value = uuid.uuid4().bytes, random_string(VALUE_LENGTH)
+            kv_store[key] = value
+
+        keys_before_delete = set(kv_store.keys())
+        deleted_key = random.choice(kv_store.keys())
+        del kv_store[deleted_key]
+        keys_after_delete = set(kv_store.keys())
+
+        self.assertEqual(len(keys_before_delete) - 1, len(keys_after_delete))
+        self.assertEqual(keys_after_delete,
+                         keys_before_delete - set([deleted_key]))
+        kv_store.close()
+
+        # Re-open and check keys again
+        kv_store = Bitcask(temp_file.name)
+        keys_after_reopen = set(kv_store.keys())
+
+        self.assertEqual(len(keys_after_delete), len(keys_after_reopen))
+        self.assertEqual(keys_after_delete, keys_after_reopen)
+
+        kv_store.close()
+
+
     # TODO: test MAXKEYSIZE (16 bits) and MAXVALSIZE (32 bits)
     # TODO: test MAXOFFSET = 16#7fffffffffffffff (max 63-bit unsigned)
 
